0c15e1e81ab727283182de582eab8ad594eadff7
Anurag Dashputre <anurag4u80@gmail.com>
2018-07-13 22:31:04 +0530

    Update NotGivingEthToken.sol

--- contracts/NotGivingEthToken.sol
+++ contracts/NotGivingEthToken.sol
@@ -1,46 +1,57 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.24;
 
 import './NotgivingEth_interface.sol';
 import './SafeMath.sol';
-
 
 contract NotGivingEthToken is NotGivingEthInterface {
     address public owner = msg.sender;
     using SafeMath for uint;
 
-    mapping(address => uint) whitebalance;
-    mapping(address => uint) blackbalance;
-    mapping(address => uint) lastblock;
+    mapping(address => uint) whiteBalance;
+    mapping(address => uint) blackBalance;
+    mapping(address => uint) lastBlock;
 
 
     //this will be called by owner, in our case trading ui
-    function transferWhiteCoin(address _sellto, uint _value){
+    function transferWhiteCoin(address _sellto, uint _value) public {
       //Burn white token of sellfrom
       //Burn Black Token of sellTo
-      require(whitebalance[msg.sender] >= _value ,'Not sufficient white token to sell');
-      require(blackbalance[_sellto] >= _value ,'Not sufficient Black token to recieve whitecoin');
-      whitebalance[msg.sender] = whitebalance[msg.sender].sub(_value);
-      blackbalance[_sellto] = blackbalance[_sellto].sub(_value);
-
+      require(whiteBalance[msg.sender] >= _value ,"Not sufficient White coin to sell");
+      require(blackBalance[_sellto] >= _value ,"Not sufficient Black coin to receive White coin");
+      whiteBalance[msg.sender] = whiteBalance[msg.sender].sub(_value);
+      blackBalance[_sellto] = blackBalance[_sellto].sub(_value);
     }
 
     //only owner can call, called from dapp
-    function spot(address _victim, address _spammer, uint _value) {
-        require(msg.sender == owner);
+    function spot(address _victim, address _spammer, uint _value) public {
+        require(_victim != address(0),"Victim cannot be address(0)");
+        require(_spammer != address(0),"Spammer cannot be address(0)");
+        require(msg.sender == owner,"You are not the Owner!!!");
         //dont allow to spot transaction for 5000 block passed
-        require(lastblock[_victim] + 5000 > block.number);
-        whitebalance[_victim] = whitebalance[_victim].add(_value);
-        blackbalance[_spammer] = blackbalance[_spammer].add(_value);
+        //require(lastBlock[_victim] + 5000 > block.number);
+        require(lastBlock[_victim] + 2 > block.number,"Allowed only every 2 blocks");
+        //require(block.number%2 == 0);
+        whiteBalance[_victim] = whiteBalance[_victim].add(_value);
+        blackBalance[_spammer] = blackBalance[_spammer].add(_value);
+        
+        lastBlock[_victim] = block.number;
+        
         emit SpottedSpam(_victim, _spammer, _value);
     }
 
     // send balance of black token, for wallets
-    function balanceOf(address _owner) constant returns (uint balance) {
-        return blackbalance[_owner];
+    function balanceOf(address _owner) constant public returns (uint balance) {
+        require(_owner != address(0),"Balance of address(0) is not possible");        
+        return blackBalance[_owner];
     }
 
     // send balance of while token, for dapp
-    function balanceOfWhite(address _owner) constant returns (uint balance) {
-        return whitebalance[_owner];
+    function balanceOfWhite(address _owner) constant public returns (uint balance) {
+        require(_owner != address(0),"Balance of address(0) is not possible");
+        return whiteBalance[_owner];
+    }
+    
+    function getBN(address _victim) returns(uint) {
+        return lastBlock[_victim];
     }
 }
